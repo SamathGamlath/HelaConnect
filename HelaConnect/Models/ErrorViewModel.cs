fb838a15a7d652b4ac2df0d2a6f339e5a0e1f7b7
Samath <samathgamlath@gmail.com>
2024-12-13 01:53:28 +0530

    Add project files.

---
+++ HelaConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelaConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
