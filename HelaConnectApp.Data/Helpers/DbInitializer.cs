e5acde599782a75c78f4eb005f5384514acacef5
Samath <samathgamlath@gmail.com>
2024-12-28 01:54:57 +0530

    Tried to load the images by making some changes to Index.cshtml and DbInitializer.cs

--- HelaConnectApp.Data/Helpers/DbInitializer.cs
+++ HelaConnectApp.Data/Helpers/DbInitializer.cs
@@ -34,7 +34,7 @@ namespace HelaConnectApp.Data.Helpers
                 var newPostWithImage = new Post()
                 {
                     Content = "This is going to be our first post which is being loaded from the database and it has been created using our test user. This post has an image",
-                    ImageUrl = "https://unsplash.com/photos/foggy-mountain-summit-1Z2niiBPg5A",
+                    ImageUrl = "https://media.licdn.com/dms/image/v2/D5603AQGXgxz1t2FuFw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1701667931397?e=1740614400&v=beta&t=E_4gh3skgvXGIO16aoMt1Fqc681zl-1kQTLa_4uf_nU",
                     NrOfReports = 0,
                     DateCreated = DateTime.UtcNow,
                     DateUpdated = DateTime.UtcNow,
