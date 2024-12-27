70d84cfab7bf29b8cefc6af6e1038f04506ef77b
Samath <samathgamlath@gmail.com>
2024-12-27 23:04:33 +0530

    Did the initial migration. Setup the new mysql database connection

--- HelaConnect/Program.cs
+++ HelaConnect/Program.cs
@@ -7,8 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //Database Configurations
-string dbConnectionString = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnectionString));
+//string dbConnectionString = builder.Configuration.GetConnectionString("Default");
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnectionString));
+
+string dbConnectionString = builder.Configuration.GetConnectionString("Connection1");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(dbConnectionString, ServerVersion.AutoDetect(dbConnectionString)));
+//options.UseMySql(dbConnectionString, ServerVersion.AutoDetect(dbConnectionString)));
 
 var app = builder.Build();
 
