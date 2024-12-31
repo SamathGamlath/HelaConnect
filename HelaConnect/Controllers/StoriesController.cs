53e3e462b4c0d22726f87b167ddae791bf40a3ff
Samath <samathgamlath@gmail.com>
2024-12-31 17:17:09 +0530

    Fixed the redirect issue

--- HelaConnect/Controllers/StoriesController.cs
+++ HelaConnect/Controllers/StoriesController.cs
@@ -13,21 +13,19 @@ namespace HelaConnect.Controllers
         {
             _context = context;
         }
-        public async Task <IActionResult> Index()
-        {
-            var allStories = await _context.Stories.Include(s => s.User).ToListAsync();
-            return View(allStories);
-        }
+
         [HttpPost]
         public async Task<IActionResult> CreateStory(StoryVM storyVM)
         {
             int loggedInUserId = 1;
+
             var newStory = new Story
             {
                 DateCreated = DateTime.UtcNow,
                 IsDeleted = false,
                 UserId = loggedInUserId
             };
+
             //Check and save the image
             if (storyVM.Image != null && storyVM.Image.Length > 0)
             {
@@ -36,17 +34,22 @@ namespace HelaConnect.Controllers
                 {
                     string rootFolderPathImages = Path.Combine(rootFolderPath, "images/stories");
                     Directory.CreateDirectory(rootFolderPathImages);
+
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(storyVM.Image.FileName);
                     string filePath = Path.Combine(rootFolderPathImages, fileName);
+
                     using (var stream = new FileStream(filePath, FileMode.Create))
                         await storyVM.Image.CopyToAsync(stream);
+
                     //Set the URL to the newPost object
                     newStory.ImageUrl = "/images/stories/" + fileName;
                 }
             }
+
             await _context.Stories.AddAsync(newStory);
             await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- HelaConnect/ViewComponents/StoriesViewComponents.cs
+++ HelaConnect/ViewComponents/StoriesViewComponents.cs
@@ -1,6 +1,20 @@
-﻿namespace HelaConnect.ViewComponents
+﻿using HelaConnectApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelaConnect.ViewComponents
 {
-    public class StoriesViewComponents
+    public class StoriesViewComponents : ViewComponent
     {
+
+        private readonly AppDbContext _context;
+        public StoriesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allStories = await _context.Stories.Include(s => s.User).ToListAsync();
+            return View(allStories);
+        }
     }
 }
