314e3b8b9e750e369b38a864b4e31ff27fff553a
Samath <samathgamlath@gmail.com>
2024-12-31 17:36:13 +0530

    Added sorting of Hashtags

--- HelaConnect/ViewComponents/HashtagsViewComponent.cs
+++ HelaConnect/ViewComponents/HashtagsViewComponent.cs
@@ -1,6 +1,12 @@
-﻿namespace HelaConnect.ViewComponents
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelaConnect.ViewComponents
 {
-    public class HashtagsViewComponent
+    public class HashtagsViewComponent:ViewComponent
     {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
     }
 }
